03fb979f6ec8ab48ad517d5fe5040ae204a3636a
Jerry <jerry830401@gmail.com>
2020-02-19 23:41:15 +0800

    1.新增 vuex 2.新增 vue-router 3.scss 全局配置 4.bootstrap-vue 配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './main.scss'
 
 Vue.config.productionTip = false
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
